d383a8d3300281a05a3134222d8ee91d6a026598
rendhive <rendhivecode@gmail.com>
2025-12-09 20:13:30 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul threading.py

---
+++ 1. Mengimpor Modul threading.py
@@ -0,0 +1,5 @@
+import threading
+
+print("Modul threading berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan threading (multi-threading) di Python.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari threading.
